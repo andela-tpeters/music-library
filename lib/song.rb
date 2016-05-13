decb203eb8b03f78031e7c57d5127db797c3577d
Andela <andela@Andelas-MacBook-Pro.local>
2016-05-13 01:31:18 +0100

    refactoring models

--- lib/song.rb
+++ lib/song.rb
@@ -2,17 +2,17 @@ require "music_library"
 
 class Song < MusicLibrary
 
-  def initialize( name, artist = nil)
+  def initialize ( name, artist = nil )
     @name = name
 
-    self.artist = artist unless artist.nil?
+    self.artist = artist if !artist.nil?
   end
 
   def artist
     @artist
   end
 
-  def artist= artist
+  def artist=(artist)
     @artist = artist
     add_song_to_artist
   end
