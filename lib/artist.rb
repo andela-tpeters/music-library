e7187ecc796da2c9e642cf0b681c88dbb5c9d1c3
Andela <andela@Andelas-MacBook-Pro.local>
2016-05-13 01:12:53 +0100

    song to artist relationship complete

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,23 @@
 require "music_library"
 
 class Artist < MusicLibrary
-	attr_accessor :songs
+	attr_reader :songs
 
 	def initialize name
 		super
 		@songs = []
 	end
+
+	def add_song song
+		add_artist_to_song(song) if song.artist.nil?
+
+		@songs << song unless @songs.include?(song)
+	end
+
+	def add_artist_to_song song
+		song.artist = self
+	end
+
+
 
 end
