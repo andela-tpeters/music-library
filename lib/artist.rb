204e227bc4d9525295304e7bf61f3fc4f90bb223
Andela <andela@Andelas-MacBook-Pro.local>
2016-05-14 15:40:56 +0100

    artist and genres working

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,20 +1,36 @@
 require "music_library"
 
 class Artist < MusicLibrary
-	attr_reader :songs
+  attr_reader :songs
 
-	def initialize(name)
-		super
-		@songs = []
-	end
+  def initialize(name)
+    super
+    @songs = []
+    @@genres = []
+    @@genre_ids = []
+  end
 
-	def add_song(song)
-		add_artist_to_song(song) if song.artist.nil?
+  def add_song(song)
+    add_artist_to_song(song) if song.artist.nil?
+    @songs << song if !@songs.include?(song)
+  end
 
-		@songs << song if !@songs.include?(song)
-	end
+  def add_artist_to_song(song)
+    song.artist = self
+  end
 
-	def add_artist_to_song(song)
-		song.artist = self
-	end
+  def genres
+    @@genres
+  end
+
+  def add_genre(genre)
+    if !@@genre_ids.include?(genre.name)
+      @@genre_ids << genre.name
+      @@genres << genre if !@@genres.include?(genre)
+    end
+
+    # binding.pry
+  end
+
+
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,31 +1,54 @@
 require "music_library"
 
 class Song < MusicLibrary
-  attr_reader :artist, :genre
+  attr_reader :artist
+  attr_accessor :name
 
-  def initialize ( name, artist = nil, genre = nil )
+  def initialize ( name, artist = nil, genre = nil)
     @name = name
 
-    self.artist = artist if !artist.nil?
-    self.genre = genre if !genre.nil?
+    add_song_to_artist(artist) if !artist.nil?
+
+    add_song_to_genre(genre) if !genre.nil?
+
+    add_artist_to_genre(artist) if !artist.nil? && !genre.nil?
+  end
+
+  def genre
+    @genre
+  end
+
+  def genres
+    @@genres
   end
 
   def artist=(artist)
     @artist = artist
-    add_song_to_artist
+    add_song_to_artist(artist) if !artist.nil?
   end
 
-  def add_song_to_artist
-    @artist.add_song(self)
+  def add_song_to_artist(artist)
+    @artist = artist
+    @artist.send(:add_song,self)
   end
 
   def genre=(genre)
     @genre = genre
-    add_song_to_genre
+    add_song_to_genre(genre) if !genre.nil?
   end
 
-  def add_song_to_genre
-    @genre.add_song(self)
+  def add_song_to_genre(genre)
+    @genre = genre
+    @artist.add_genre(genre) if !@artist.nil?
+    @genre.add_song(self) if !genre.nil?
+  end
+
+  def add_genre_to_artist(genre)
+    @artist.add_genre(genre)
+  end
+
+  def add_artist_to_genre(artist)
+    @genre.add_artist(artist)
   end
 
 
