c7360bfe82e2139d0b89d668ffd5d6352496dd03
Andela <andela@Andelas-MacBook-Pro.local>
2016-05-24 17:54:19 +0100

    artist model refactored

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,35 +1,28 @@
-require "music_library"
+class Artist
+  attr_accessor :name
+  
+  include Concerns::InstanceMethods
+  extend Concerns::ClassMethods
+  extend Concerns::Findable
 
-class Artist < MusicLibrary
-  attr_reader :songs
+  @@all = []
+  @@songs = {}
+  @@genres = {}
 
   def initialize(name)
-    super
-    @songs = []
-    @@genres = []
-    @@genre_ids = []
+    @name = name
+    @@songs[@name] = []
+    @@genres[@name] = []
   end
 
-  def add_song(song)
-    add_artist_to_song(song) if song.artist.nil?
-    @songs << song if !@songs.include?(song)
+  def add_artist_to_song(song_object)
+    song_object.artist = self if song_object.artist.nil?
   end
 
-  def add_artist_to_song(song)
-    song.artist = self
-  end
-
-  def genres
-    @@genres
-  end
-
-  def add_genre(genre)
-    if !@@genre_ids.include?(genre.name)
-      @@genre_ids << genre.name
-      @@genres << genre if !@@genres.include?(genre)
-    end
-
-    # binding.pry
+  def add_song(song_object)
+    add_artist_to_song(song_object) if self.class == Artist
+    songs << song_object unless songs.include?(song_object)
+    songs
   end
 
 
