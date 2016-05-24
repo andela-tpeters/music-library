ec9449b7891d9bb1e2035c6c0ee57d7c822f1a59
Andela <andela@Andelas-MacBook-Pro.local>
2016-05-24 16:59:02 +0100

    initial commit

--- lib/genre.rb
+++ lib/genre.rb
@@ -7,9 +7,22 @@ class Genre < MusicLibrary
     super
 
     @songs = []
+    @@artists = []
+    @@artists_ids = []
   end
 
   def add_song(song)
     @songs << song if !@songs.include?(song)
+  end
+
+  def artists
+    @@artists
+  end
+
+  def add_artist(artist)
+    if !@@artists_ids.include?(artist.name)
+      @@artists_ids << artist.name
+      @@artists << artist if !@@artists.include?(artist)
+    end
   end
 end
