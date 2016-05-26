8af0503797ca528427aaf0d35fa33b5d1a6a950f
Tijesunimi Peters <tijesunimi.peters@andela.com>
2016-05-26 11:35:12 +0100

    rf messages

--- lib/messages.rb
+++ lib/messages.rb
@@ -22,9 +22,9 @@ class Messages
 
   def available_commands
     puts """
-      =========================================================
-                          Available Commands
-      =========================================================""".yellow
+	  =========================================================
+	                  Available Commands
+	  =========================================================""".yellow
   end
 
   def progress_bar
@@ -39,14 +39,15 @@ class Messages
 
   def display_commands
       available_commands
-      puts "\tCommands\t| Description"
-      puts "\t--------------------------------"
+      puts """
+	    	Commands \t| \tDescription
+	    ------------------------------------------"""
       commands.each do |key, value|
       	if key == 'help' || key == 'exit'
-      		puts "\t" << key.green << "\t\t| " << value.to_s.gsub("_"," ").white
+      		puts """		#{key.green} \t\t| \t#{value.to_s.gsub("_"," ").white}"""
       		next
       	end
-        puts "\t" << key.green << "\t| " << value.to_s.gsub("_"," ").white
+        puts """		#{key.green} \t| \t#{value.to_s.gsub("_"," ").white}"""
       end
   end
 
@@ -54,9 +55,9 @@ class Messages
     progress_bar
     system 'clear'
     puts """
-    =============================================================
-                        Welcome Music Library
-    =============================================================""".white
+	=============================================================
+	                    Welcome Music Library
+	=============================================================""".white
     display_commands
   end
 
@@ -79,6 +80,15 @@ class Messages
 
   def print_song_number_caption
     print "Please Choose a song number: ".yellow
+  end
+
+  def no_command_error_message
+  	put("""Error:
+  		The command action is not available: 
+  		Please check the spellings and try again
+  		or run #{"\"help\"".white} to see list of available commands
+
+  	""",'red')
   end
   
   
